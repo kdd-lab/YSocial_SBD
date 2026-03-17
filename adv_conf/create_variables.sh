714cbc874f157b2ae57925d1496b3ab0174bca55
GiulioRossetti <giulio.rossetti@gmail.com>
2026-03-17 14:00:11 +0100

    :arrow_up: cleaning

--- adv_conf/create_variables.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "UID=$(id -u)" > .env
-echo "GID=$(id -g)" >> .env
-echo "PWD=$(pwd)" >> .env
--- adv_conf/recreate_and_start.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
- ./docker-compose -f docker-compose_reverse.yml build
- ./docker-compose -f docker-compose_reverse.yml up -d
- #./docker-compose -f docker-compose_reverse.yml down
